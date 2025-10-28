02e249ae7ad651b032a4f52d5744143190022e35
Magnet <phatthansn.charoenphol@gmail.com>
2025-10-28 19:53:28 +0700

    refactor: restructure project folder

--- internal/infrastructure/http/response/success.go
+++ internal/infrastructure/http/response/success.go
@@ -14,16 +14,6 @@ type APIResponse struct {
 	Pagination *Pagination `json:"pagination,omitempty"`
 }
 
-type APIError struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
-	Details any    `json:"details,omitempty"`
-}
-
-type ErrorResponse struct {
-	Error APIError `json:"error"`
-}
-
 func OK(c *fiber.Ctx, data any) error {
 	return c.Status(fiber.StatusOK).JSON(APIResponse{Data: data})
 }
@@ -40,14 +30,3 @@ func NoContent(c *fiber.Ctx) error {
 	return c.SendStatus(fiber.StatusNoContent)
 }
 
-func BadRequest(c *fiber.Ctx, msg string, details any) error {
-	return c.Status(fiber.StatusBadRequest).JSON(ErrorResponse{
-		Error: APIError{Code: "BAD_REQUEST", Message: msg, Details: details},
-	})
-}
-
-func Unauthorized(c *fiber.Ctx, msg string) error {
-	return c.Status(fiber.StatusUnauthorized).JSON(ErrorResponse{
-		Error: APIError{Code: "UNAUTHORIZED", Message: msg},
-	})
-}
