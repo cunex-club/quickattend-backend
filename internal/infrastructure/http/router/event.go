a6f43d5a74db1b86049f391a2a8925af7ff7b68a
naxocist <subforwork@gmail.com>
2025-12-26 23:32:00 +0700

    feat(event): setup router for events

--- internal/infrastructure/http/router/event.go
+++ internal/infrastructure/http/router/event.go
@@ -2,11 +2,11 @@ package router
 
 import (
 	"github.com/cunex-club/quickattend-backend/internal/infrastructure/http/handler"
+	"github.com/cunex-club/quickattend-backend/internal/infrastructure/http/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
-func EventRoutes(r fiber.Router, h *handler.AllOfHandler) {
-	event := r.Group("/event")
-
+func EventRoutes(r fiber.Router, h *handler.AllOfHandler, mw *middleware.Middleware) {
+	event := r.Group("/event", mw.AuthRequired())
 	event.Delete("/:id", h.EventHandler.EventDelete)
 }
