888f3a65fb92d5fe58c951ccef69dbcb43644431
Dawit <dawitsurithpinyo@gmail.com>
2026-01-03 20:31:29 +0700

    fix: change events handler to convert `ref_id` to to float64 before converting to uint64

--- internal/infrastructure/http/handler/event.go
+++ internal/infrastructure/http/handler/event.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"strconv"
+
 	"github.com/cunex-club/quickattend-backend/internal/infrastructure/http/response"
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,7 +13,11 @@ type EventHandler interface {
 
 func (h *Handler) GetEvents(c *fiber.Ctx) error {
 	params := c.Queries()
-	refID := c.Locals("ref_id").(uint64)
+	refIDFloat, errStrToFloat := strconv.ParseFloat(c.Locals("ref_id").(string), 64)
+	if errStrToFloat != nil {
+		return response.SendError(c, 400, response.ErrBadRequest, "ref_id should be able to convert to float64")
+	}
+	refID := uint64(refIDFloat)
 
 	res, pagination, err := h.Service.Event.GetEventsService(refID, params, c.UserContext())
 	if err != nil {
