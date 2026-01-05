0ed0fa3d2616ee2fc325ee70cd3225219638692e
Dawit <dawitsurithpinyo@gmail.com>
2026-01-05 09:22:59 +0700

    feat: create response DTO for GET /participant/:qrcode

---
+++ internal/dto/response/event.go
@@ -0,0 +1,17 @@
+package response
+
+import "time"
+
+type GetParticipantRes struct {
+	FirstnameTH  string    `json:"firstname_th"`
+	SurnameTH    string    `json:"surname_th"`
+	TitleTH      string    `json:"title_th"`
+	FirstnameEN  string    `json:"firstname_en"`
+	SurnameEN    string    `json:"surname_en"`
+	TitleEN      string    `json:"title_en"`
+	RefID        uint64    `json:"ref_id"`
+	Organization string    `json:"organization"`
+	CheckInTime  time.Time `json:"check_in_time"`
+	Status       string    `json:"status"`
+	Code         string    `json:"code"`
+}
