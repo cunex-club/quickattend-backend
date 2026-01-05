df72b31674af68134bd2bc601ddecc062c4598d8
Dawit <dawitsurithpinyo@gmail.com>
2026-01-05 09:17:22 +0700

    feat: init GET /participant/:qrcode service

--- internal/service/event.go
+++ internal/service/event.go
@@ -1,4 +1,16 @@
 package service
 
+import (
+	"context"
+
+	dtoRes "github.com/cunex-club/quickattend-backend/internal/dto/response"
+	"github.com/cunex-club/quickattend-backend/internal/infrastructure/http/response"
+)
+
 type EventService interface {
+	GetParticipantService(code string, ctx context.Context) (*[]dtoRes.GetParticipantRes, *response.APIError)
+}
+
+func (s *service) GetParticipantService(code string, ctx context.Context) (*[]dtoRes.GetParticipantRes, *response.APIError) {
+	return nil, nil
 }
