3251b3ba7b6de48f218119462436b29b2362cbb8
Dawit <dawitsurithpinyo@gmail.com>
2026-01-05 09:18:11 +0700

    feat: init GET /participant/:qrcode handler

--- internal/infrastructure/http/handler/event.go
+++ internal/infrastructure/http/handler/event.go
@@ -1,4 +1,19 @@
 package handler
 
+import (
+	"github.com/cunex-club/quickattend-backend/internal/infrastructure/http/response"
+	"github.com/gofiber/fiber/v2"
+)
+
 type EventHandler interface {
+	GetParticipantHandler(c *fiber.Ctx) error
+}
+
+func (h *Handler) GetParticipantHandler(c *fiber.Ctx) error {
+	code := c.Params("qrcode")
+	res, err := h.Service.Event.GetParticipantService(code, c.UserContext())
+	if err != nil {
+		return response.SendError(c, err.Status, err.Code, err.Message)
+	}
+	return response.OK(c, res)
 }
