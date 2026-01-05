df94f877be000eb246a9850380f1ea5dab5c99ae
Dawit <dawitsurithpinyo@gmail.com>
2026-01-05 15:14:58 +0700

    feat: add struct and type for response body of CU NEX `GetDynamicQRinfo` request

--- internal/entity/cunex.go
+++ internal/entity/cunex.go
@@ -9,3 +9,24 @@ type CUNEXUserResponse struct {
 	FirstnameEN string `json:"firstNameEN"`
 	LastNameEN  string `json:"lastNameEN"`
 }
+
+type CUNEXGetQRErrorResponse struct {
+	ErrorCode string `json:"errorCode"`
+	Message   string `json:"message"`
+}
+
+type UserTypes string
+
+const (
+	STUDENTS UserTypes = "student"
+	STAFFS   UserTypes = "staff"
+)
+
+type CUNEXGetQRSuccessResponse struct {
+	UserType     UserTypes `json:"userType"`
+	RefId        string    `json:"refId"`
+	FirstName    string    `json:"firstName"`
+	LastName     string    `json:"lastName"`
+	Organization string    `json:"organization"`
+	PhotoBase64  string    `json:"photoBase64"`
+}
