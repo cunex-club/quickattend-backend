0dee0ffd0d1a06b92b7af5cd276584b07077ee77
Dawit <dawitsurithpinyo@gmail.com>
2026-01-06 11:41:42 +0700

    feat: refactor and fix incorrect queries in repo of GET /participant/:qrcode

--- internal/repository/event.go
+++ internal/repository/event.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 
 	"github.com/cunex-club/quickattend-backend/internal/entity"
 	"gorm.io/datatypes"
@@ -13,7 +14,7 @@ type EventRepository interface {
 	// Check if user has already check in to the event
 	GetParticipantCheckParticipation(ctx context.Context, eventId datatypes.UUID, refID uint64) (found bool, err error)
 	// Check if user is in whitelist / allowed org or faculty of the event
-	GetParticipantCheckAccess(ctx context.Context, orgCode int64, attendanceType string, eventId datatypes.UUID) (allow bool, err error)
+	GetParticipantCheckAccess(ctx context.Context, orgCode int64, refID uint64, attendanceType string, eventId datatypes.UUID) (allow bool, err error)
 }
 
 func (r *repository) GetParticipantUserInfoAndAttendanceType(ctx context.Context, eventId datatypes.UUID, refID uint64) (*entity.User, string, error) {
@@ -39,11 +40,10 @@ func (r *repository) GetParticipantCheckParticipation(ctx context.Context, event
 	withCtx := r.db.WithContext(ctx)
 
 	var found bool
-	checkParticipantErr := withCtx.Select(`EXISTS (
+	checkParticipantErr := withCtx.Raw(`SELECT EXISTS (
 				SELECT 1 FROM event_participants
-				WHERE event_participants.participant_ref_id = ?
-				AND event_participants.event_id = ?
-			)`, refID, eventId).Scan(&found).Error
+				WHERE participant_ref_id = ? AND event_id = ?
+			) AS subQuery`, refID, eventId).Scan(&found).Error
 	if checkParticipantErr != nil {
 		return false, checkParticipantErr
 	}
@@ -51,56 +51,35 @@ func (r *repository) GetParticipantCheckParticipation(ctx context.Context, event
 	return found, nil
 }
 
-func (r *repository) GetParticipantCheckAccess(ctx context.Context, orgCode int64, attendanceType string, eventId datatypes.UUID) (bool, error) {
+func (r *repository) GetParticipantCheckAccess(ctx context.Context, orgCode int64, refID uint64, attendanceType string, eventId datatypes.UUID) (bool, error) {
 	withCtx := r.db.WithContext(ctx)
+	var found bool
 
-	if attendanceType == string(entity.FACULTIES) {
-		var facultyNoList []int64
-		getListErr := withCtx.Select("event_allowed_faculties.faculty_no").
-			Where("event_allowed_faculties.event_id = ?", eventId).
-			Scan(&facultyNoList).Error
-		if getListErr != nil {
-			return false, getListErr
+	switch attendanceType {
+	case string(entity.FACULTIES):
+		checkErr := withCtx.Raw(`SELECT EXISTS (
+			SELECT 1 FROM event_allowed_faculties
+			WHERE event_id = ? AND faculty_no = ?
+		) AS subQuery`, eventId, orgCode).Scan(&found).Error
+		if checkErr != nil {
+			return false, checkErr
 		}
 
-		if len(facultyNoList) == 0 {
-			return false, nil
-		} else {
-			allowed := false
-			for _, no := range facultyNoList {
-				if orgCode == no {
-					allowed = true
-				}
-			}
-			if !allowed {
-				return false, nil
-			}
+		return found, nil
+
+	case string(entity.WHITELIST):
+		checkErr := withCtx.Raw(`SELECT EXISTS (
+			SELECT 1 FROM event_whitelists
+			WHERE event_id = ? AND attendee_ref_id = ?
+		) AS subQuery`, eventId, refID).Scan(&found).Error
+		if checkErr != nil {
+			return false, checkErr
 		}
 
-		return true, nil
+		return found, nil
+
+	default:
+		// should not happen
+		return false, errors.New("attendanceType is neither 'FACULTIES' nor 'WHITELISTS'")
 	}
-
-	var whiteList []uint64
-	getListErr := withCtx.Select("event_whitelists.attendee_ref_id").
-		Where("event_whitelists.event_id = ?", eventId).
-		Scan(&whiteList).Error
-	if getListErr != nil {
-		return false, getListErr
-	}
-
-	if len(whiteList) == 0 {
-		return false, nil
-	} else {
-		allowed := false
-		for _, no := range whiteList {
-			if orgCode == int64(no) {
-				allowed = true
-			}
-		}
-		if !allowed {
-			return false, nil
-		}
-	}
-
-	return true, nil
 }
