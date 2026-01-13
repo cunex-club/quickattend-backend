c60cf8ff5a1386f194bdfe8bf083c944cd85cad3
Dawit <dawitsurithpinyo@gmail.com>
2026-01-13 14:57:44 +0700

    feat(get-an-event): add service

--- internal/service/event.go
+++ internal/service/event.go
@@ -1,4 +1,48 @@
 package service
 
+import (
+	"context"
+
+	dtoRes "github.com/cunex-club/quickattend-backend/internal/dto/response"
+	"github.com/cunex-club/quickattend-backend/internal/infrastructure/http/response"
+	"github.com/google/uuid"
+	"gorm.io/datatypes"
+	"gorm.io/gorm"
+)
+
 type EventService interface {
+	GetOneEventService(eventIdStr string, ctx context.Context) (res *dtoRes.GetOneEventRes, err *response.APIError)
+}
+
+func (s *service) GetOneEventService(eventIdStr string, ctx context.Context) (*dtoRes.GetOneEventRes, *response.APIError) {
+	eventIdErr := uuid.Validate(eventIdStr)
+	if eventIdErr != nil {
+		return nil, &response.APIError{
+			Code:    response.ErrBadRequest,
+			Message: "Invalid URL path parameter 'id'",
+			Status:  400,
+		}
+	}
+	eventId := datatypes.UUID(datatypes.BinUUIDFromString(eventIdStr))
+
+	res, err := s.repo.Event.GetOneEvent(eventId, ctx)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, &response.APIError{
+				Code:    response.ErrNotFound,
+				Message: "Event with this id not found",
+				Status:  404,
+			}
+		} else {
+			s.logger.Error().Err(err).
+				Str("function", "EventRepository.GetOneEvent")
+			return nil, &response.APIError{
+				Code:    response.ErrInternalError,
+				Message: "Internal DB error",
+				Status:  500,
+			}
+		}
+	}
+
+	return res, nil
 }
