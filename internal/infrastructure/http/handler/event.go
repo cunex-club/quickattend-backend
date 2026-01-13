b3e8bdb5f76b171c1894e71300b0ff47e14ee6bb
Dawit <dawitsurithpinyo@gmail.com>
2026-01-13 14:55:24 +0700

    feat(get-an-event): add handler

--- internal/infrastructure/http/handler/event.go
+++ internal/infrastructure/http/handler/event.go
@@ -1,4 +1,21 @@
 package handler
 
+import (
+	"github.com/cunex-club/quickattend-backend/internal/infrastructure/http/response"
+	"github.com/gofiber/fiber/v2"
+)
+
 type EventHandler interface {
+	GetOneEventHandler(*fiber.Ctx) error
+}
+
+func (h *Handler) GetOneEventHandler(c *fiber.Ctx) error {
+	eventIdStr := c.Params("id")
+
+	res, err := h.Service.Event.GetOneEventService(eventIdStr, c.UserContext())
+	if err != nil {
+		return response.SendError(c, err.Status, err.Code, err.Message)
+	}
+
+	return response.OK(c, res)
 }
