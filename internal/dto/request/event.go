1f0611af75275dca7dfe4b11f84f232a03cf5a78
Dawit <dawitsurithpinyo@gmail.com>
2026-01-15 21:19:55 +0700

    feat(post-participant): add scanned location X and Y to req body type

--- internal/dto/request/event.go
+++ internal/dto/request/event.go
@@ -1,5 +1,7 @@
 package response
 
 type PostParticipantReqBody struct {
-	EventId string `json:"event_id"`
+	EventId          string  `json:"event_id"`
+	ScannedLocationX float64 `json:"scanned_location_x"`
+	ScannedLocationY float64 `json:"scanned_location_y"`
 }
