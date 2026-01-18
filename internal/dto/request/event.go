65bbfe2458e1f139430a4ad6e376dd688977e531
Dawit <dawitsurithpinyo@gmail.com>
2026-01-18 18:16:07 +0700

    feat(post-participant): add scanned location to request body

--- internal/dto/request/event.go
+++ internal/dto/request/event.go
@@ -2,6 +2,6 @@ package response
 
 type PostParticipantReqBody struct {
 	EventId          string  `json:"event_id"`
-	ScannedLocationX float64 `json:"scanned_location_x"`
-	ScannedLocationY float64 `json:"scanned_location_y"`
+	ScannedLocationX float64 `json:"scanned_location_long"`
+	ScannedLocationY float64 `json:"scanned_location_lat"`
 }
