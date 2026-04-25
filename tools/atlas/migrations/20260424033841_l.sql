336999d0510bc31c301beda5370ca06930dc9cf6
Dawit <dawitsurithpinyo@gmail.com>
2026-04-25 17:27:23 +0700

    chore: remove migration files

--- tools/atlas/migrations/20260424033841_l.sql
+++
@@ -1,2 +0,0 @@
--- Modify "event_user_pendings" table
-ALTER TABLE "event_user_pendings" DROP CONSTRAINT "unique_user_and_event_pendings", DROP COLUMN "user_id", ADD COLUMN "user_ref_id" bigint NOT NULL, ADD CONSTRAINT "unique_user_and_event_pendings" UNIQUE ("user_ref_id", "event_id");
