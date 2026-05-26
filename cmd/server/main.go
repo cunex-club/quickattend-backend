aa77ea7545bdcf15d33c876b6de9d96295236dfc
Magnet <phatthansn.charoenphol@gmail.com>
2026-05-26 20:13:27 +0700

    fix: inject service into resolver

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -47,7 +47,9 @@ func main() {
 		mw.RequestLogger(),
 	)
 
-	gqlResolver := &gql.Resolver{}
+	gqlResolver := &gql.Resolver{
+		Service: &services,
+	}
 
 	router.SetupRoutes(app, handlers, mw, gqlResolver)
 	log.Info().Msg("Starting server on :8000")
--- internal/infrastructure/http/handler/graphql/server.go
+++ internal/infrastructure/http/handler/graphql/server.go
@@ -1,12 +1,26 @@
 package graphql
 
 import (
+	"fmt"
+
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 )
 
 func GraphQLServer(resolver *Resolver) *handler.Server {
+	if resolver == nil {
+		panic(fmt.Errorf("graphql resolver is nil"))
+	}
+
+	if resolver.Service == nil {
+		panic(fmt.Errorf("graphql resolver service is nil"))
+	}
+
+	if resolver.Service.Dashboard == nil {
+		panic(fmt.Errorf("dashboard service is nil"))
+	}
+
 	srv := handler.New(NewExecutableSchema(Config{Resolvers: resolver}))
 
 	srv.AddTransport(transport.Options{})
